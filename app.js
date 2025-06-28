fbc4bee5101354adbeafa382bf16594c7495770e
Farooq Ahmed <03174748082ali@gmail.com>
2025-06-28 07:14:18 -0700

    work completed

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 //     Number Methods
 
-//
+// string convert to number
+
+// Number (value)- convert to string
+
+//console.log(Number("123"));     // 123
+//console.log(Number("abc"));     // NaN   Alphabet not a number 
+
+
+
+// ParseInt(string)
+
+// String (value) convert to Number
+
+//console.log(parseInt("42px")); // 42 String convert to number
+//console.log(parseInt("3.13")); // 3 string convert to number
+
+
+
+// ParseFloat(String)
+
+// String (value) convert to number a
+
+//console.log(parseFloat("3.14"));     // 3.14
+//console.log(parseFloat("12.34px"));  //  12.34
+
+
+
+
+//toString()-convert number to string
+
+//let num = 255;
+//console.log(num.toString());    // 255 convert to nummber
+//console.log(num.toString(16));  // 16 convert to "ff" (hex)
+
+
+// Valueof()Get the Primitive value (rarely needed)
+
+//let x = new Number(42)
+//console.log(x.valueOf());
